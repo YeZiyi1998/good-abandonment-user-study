24437e024b1802546c2c728d9ef859c5ec816477
yeziyi <yeziyi1998@gmail.com>
2021-07-01 09:10:23 +0800

    data

---
+++ final_choose/part4_merge.py
@@ -0,0 +1,18 @@
+import json
+
+dis_select_id = [40, 44, 47, 50, 55, 63, 64, 80]
+
+def get_raw():
+    raw_file = json.load(open('../random_data/raw_90.json'))
+    lost_after_qid_number = {}
+    after_qid = []
+    for i in range(4, -1, -1):
+        lost_after_qid_number[i] = 18 - len([item for item in raw_file[i * 18: i * 18 + 18] if item['id'] not in dis_select_id])
+    for i in range(len(raw_file)):
+        if i not in dis_select_id:
+            after_qid.append(raw_file[i])
+    return raw_file, after_qid, lost_after_qid_number
+
+a1 = json.load(open('../random_data/select_left.json'))
+raw_file, after_qid, lost_after_qid_number = get_raw()
+json.dump(after_qid + a1, open('../random_data/select_90.json', 'w'))
